43083486a234fd62d07f3985bba30314d86c818d
koraykarakus <koraykarakus@yahoo.com>
2023-01-05 15:20:58 +0300

    Misspelling in language/tr/install is corrected.

--- language/tr/INSTALL.php
+++ language/tr/INSTALL.php
@@ -1,6 +1,6 @@
 <?php
 /**
- *  2Moons 
+ *  2Moons
  *   by Jan-Otto Kröpke 2009-2016
  *
  * For the full copyright and license information, please view the LICENSE
@@ -22,108 +22,105 @@
 
 $LNG['back']					= 'Geri';
 $LNG['continue']				= 'Devam Et';
-$LNG['continueUpgrade']			= 'Guncellestir!';
-$LNG['login']					= 'Giris';
+$LNG['continueUpgrade']			= 'Güncelleştir!';
+$LNG['login']					= 'Giriş';
 
-$LNG['menu_intro']				= 'Tanitim';
+$LNG['menu_intro']				= 'Tanıtım';
 $LNG['menu_install']			= 'Kurulum';
 $LNG['menu_license']			= 'Lisans';
-$LNG['menu_upgrade']			= 'Guncelle';
+$LNG['menu_upgrade']			= 'Güncelle';
 
 $LNG['title_install']			= 'Kurulum';
 
 $LNG['intro_lang']				= 'Dil';
-$LNG['intro_install']			= 'To installation';
-$LNG['intro_welcome']			= 'Hosgeldin yeni 2Moons Kullanicisi!';
-$LNG['intro_text']				= '2moons en iyi ogame projelerinden birisidir.<br>2Moons mevcut gelistirilmis en yeni ve guvenilir Xnova versiyonudur. 2Moons gerek kullanim kolayligi, gerek kod esnekligi, dinamizmi, kod kalitesi ve islevleri ile goz almaktadir. Her zaman sizin beklentilerinizden daha iyi olmaya calistik. <br><br> Kurulumdaki direktifler kurulum esnasinda size rehberlik edecektir. Her sorun ve problemde, bize danismak icin kesinlikle tereddut etmeyin!<br><br>The 2Moons bir acik kod uygulamasidir ve GNU GPL v3 lisanslidir. Lisans hakkinda bilgi edinmek icin asagidaki "Lisans" linkine tiklayabilirsiniz.  <br><br> Kurulum islemine baslamadan once sisteminiz 2moons kurmak icin gerekli ozelliklere sahip olup olmadigi test edilecektir. ';
+$LNG['intro_install']			= 'Kuruluma git';
+$LNG['intro_welcome']			= 'Hoşgeldin yeni 2Moons Kullanıcısı!';
+$LNG['intro_text']				= '2moons en iyi ogame projelerinden birisidir.<br> 2Moons gerek kullanim kolayligi, gerek kod esnekligi, dinamizmi, kod kalitesi ve islevleri ile goz almaktadir. Her zaman sizin beklentilerinizden daha iyi olmaya calistik. <br><br> Kurulumdaki direktifler kurulum esnasinda size rehberlik edecektir. Her sorun ve problemde, bize danismak icin kesinlikle tereddut etmeyin!<br><br>The 2Moons bir acik kod uygulamasidir ve GNU GPL v3 lisanslidir. Lisans hakkinda bilgi edinmek icin asagidaki "Lisans" linkine tiklayabilirsiniz.  <br><br> Kurulum islemine baslamadan once sisteminiz 2moons kurmak icin gerekli ozelliklere sahip olup olmadigi test edilecektir. ';
 $LNG['intro_upgrade_head']		= 'Sisteminizde 2moons mevcut kurulu mu?';
 $LNG['intro_upgrade_text']		= '<p>Sisteminizde 2Moons kurulu, kolay bir guncelleme istermisiniz?</p><p>Buraya tiklayarak sisteminizin eski veritabanini bir kac tiklama ile guncelleyebilirsiniz.!</p>';
 
 
-$LNG['upgrade_success']			= 'Guncelleme basari ile tamamlandi. Veritabaniniz su an surum  %s icin uygun.';
-$LNG['upgrade_nothingtodo']		= 'Herhangi bir eylem gerekmiyor. Veritabani surum  %s icin uygun.';
+$LNG['upgrade_success']			= 'Güncelleme başarı ile tamamlandı. Veritabanınız şu an sürüm %s için uygun.';
+$LNG['upgrade_nothingtodo']		= 'Herhangi bir eylem gerekmiyor. Veritabanı sürüm %s için uygun.';
 $LNG['upgrade_back']			= 'Geri';
-$LNG['upgrade_intro_welcome']	= 'Veritabani guncellemeye hosgeldiniz!';
-$LNG['upgrade_available']		= 'Veritabaniniz icin gerekli guncellemeler! Veritabaninizin su anki surumu  %s ve surum %s guncellenebilir.<br><br Lutfen asagidaki menuden ilk SQL guncellemesini tiklayiniz:';
-$LNG['upgrade_notavailable']	= 'Mevcut surum %s zaten son versiyon.';
-$LNG['upgrade_required_rev']	= 'Guncelleme programi versiyon  r2579 (2Moons v1. 7) yada ust versiyonlari icin uygulanabilir.';
+$LNG['upgrade_intro_welcome']	= 'Veritabanı güncellemeye hoşgeldiniz!';
+$LNG['upgrade_available']		= 'Veritabanınız için gerekli güncellemeler! Veritabanınızın şu anki sürümü %s ve sürüm %s güncellenebilir.<br><br Lütfen aşagıdaki menüden ilk SQL güncellemesini tıklayınız:';
+$LNG['upgrade_notavailable']	= 'Mevcut sürüm %s zaten son versiyon.';
+$LNG['upgrade_required_rev']	= 'Güncelleme programı versiyon  r2579 (2Moons v1. 7) yada üst versiyonlari icin uygulanabilir.';
 
 
-$LNG['licence_head']			= 'Lisans Kosullari';
-$LNG['licence_desc']			= 'Lutfen asagidaki lisans kosullarini iyice okuyunuz.';
-$LNG['licence_accept']			= '2Moons kurulumuna devam etmek icin, lisans sartlarini kabul etmeniz gerekmektedir. ';
-$LNG['licence_need_accept']		= 'Eger kuruluma devam etmek istiyorsaniz lisans kosullarini kabul etmelisiniz.';
+$LNG['licence_head']			= 'Lisans Koşulları';
+$LNG['licence_desc']			= 'Lütfen asagidaki lisans koşullarını iyice okuyunuz.';
+$LNG['licence_accept']			= '2Moons kurulumuna devam etmek icin, lisans şartlarini kabul etmeniz gerekmektedir. ';
+$LNG['licence_need_accept']		= 'Eğer kuruluma devam etmek istiyorsanız lisans koşullarını kabul etmelisiniz.';
 
-$LNG['req_head']				= 'System Gereksinimleri';
-$LNG['req_desc']				= 'Kuruluma baslamadan once 2moons sistemin kurulum icin uygun olup olmadigini test edecek. Sonuclari dikkatlice okumaniz tavsiye edilir ve butun testleri gecmeden bir sonraki adima gecmeyiniz';
+$LNG['req_head']				= 'Sistem Gereksinimleri';
+$LNG['req_desc']				= 'Kuruluma baslamadan önce, 2moons sistemin kurulum icin uygun olup olmadigini test edecek. Sonuçları dikkatlice okumanız tavsiye edilir ve bütün testleri geçmeden bir sonraki adıma geçmeyiniz';
 $LNG['reg_yes']					= 'Evet';
-$LNG['reg_no']					= 'Hayir';
+$LNG['reg_no']					= 'Hayır';
 $LNG['reg_found']				= 'Bulundu';
-$LNG['reg_not_found']			= 'Bulunamadi';
+$LNG['reg_not_found']			= 'Bulunamadı';
 $LNG['reg_writable']			= 'Kaydedilebilir';
 $LNG['reg_not_writable']		= 'Kaydedilemez';
 $LNG['reg_file']				= 'Dosya &raquo;%s&laquo; Kaydedilebilir mi?';
-$LNG['reg_dir']					= 'Klasor &raquo;%s&laquo; Kaydedilebilir mi?';
+$LNG['reg_dir']					= 'Klasör &raquo;%s&laquo; Kaydedilebilir mi?';
 $LNG['req_php_need']			= 'Mevcut script dili/versiyonu &raquo;PHP&laquo;';
-$LNG['req_php_need_desc']		= '<strong>Gereken:</strong> — PHP 2moons yazilim dilidir. Bu sebeple, butun fonksiyonlarin tam calisabilmesi icin mevcut PHP versiyonu 5.2.5 yada ustu olmasi gereklidir. ';
-$LNG['reg_gd_need']				= 'Yuklu GD PHP Script var mi? &raquo;gdlib&laquo;';
-$LNG['reg_gd_desc']				= '<strong>Opsiyonel</strong> — Grapfik isleme kutuphanesi &raquo;gdlib&laquo; sistemdeki dinamik resimlerin olusturma isinden sorumludur. Bu olmadan sistemdeki bazi fonksiyonlar calismayabilir.';
+$LNG['req_php_need_desc']		= '<strong>Gereken:</strong> — PHP 2moons yazılım dilidir. Bu sebeple, bütün fonksiyonlarin tam çalısabilmesi için mevcut PHP versiyonu 5.2.5 yada üstü olması gereklidir. ';
+$LNG['reg_gd_need']				= 'Yuklu GD PHP Script var mı? &raquo;gdlib&laquo;';
+$LNG['reg_gd_desc']				= '<strong>Opsiyonel</strong> — Grafik işleme kütüphanesi &raquo;gdlib&laquo; sistemdeki dinamik resimlerin oluşturma işinden sorumludur. Bu olmadan sistemdeki bazı fonksiyonlar calişmayabilir.';
 $LNG['reg_mysqli_active']		= 'Extension support &raquo;MySQLi&laquo;';
-$LNG['reg_mysqli_desc']			= '<strong>Gereken</strong> — PHP de MYSQL icin destek olmali. Eger girilen hic bir veritabani uygun degilse domain saglayiciniz ile gorusmeli yada PHP dosyalarini gozden gecirmelisiniz.';
-$LNG['reg_json_need']			= ' &raquo;JSON&laquo; uzantisi uygun mu?';
+$LNG['reg_mysqli_desc']			= '<strong>Gereken</strong> — PHP de MYSQL icin destek olmali. Eğer girilen hiç bir veritabanı uygun değilse domain sağlayıcınız ile görüşmeli yada PHP dosyalarını gözden geçirmelisiniz.';
+$LNG['reg_json_need']			= ' &raquo;JSON&laquo; uzantısı uygun mu?';
 $LNG['reg_iniset_need']			= 'PHP fonksiyonu &raquo;ini_set&laquo; uygun mu?';
 $LNG['reg_global_need']			= 'Globallerin kaydi aktif mi?';
-$LNG['reg_global_desc']			= '2Moons globaller aktif olsun olsun olmasin calisacaktir. Ama, eger mumkunse guvenlik onlemleri bunu inaktif yapmaniz onerilir.';
+$LNG['reg_global_desc']			= '2Moons globaller aktif olsun ya da olmasın çalışacaktır. Ama, eğer mümkünse güvenlik önlemleri için bunu inaktif yapmanız önerilir.';
 $LNG['req_ftp_head']			= 'FTP bilgilerini giriniz';
-$LNG['req_ftp_desc']			= 'FTP bilgilerinizi giriniz ve 2moons otomatik olarak problemleri duzeltsin. Alternatif olarak, kendiniz yetki verebilirsiniz.';
+$LNG['req_ftp_desc']			= 'FTP bilgilerinizi giriniz ve 2moons otomatik olarak problemleri düzeltsin. Alternatif olarak, kendiniz yetki verebilirsiniz.';
 $LNG['req_ftp_host']			= 'Domain Adi(HostName)';
-$LNG['req_ftp_username']		= 'Kullanici Adi';
-$LNG['req_ftp_password']		= 'Sifre';
-$LNG['req_ftp_dir']				= '2Moons klasoru';
-$LNG['req_ftp_send']			= 'Gonder';
-$LNG['req_ftp_error_data']		= 'Verilen bilgiler ile FTP sunucusuna baglanilamadi. Gorev basarisiz oldu.';
-$LNG['req_ftp_error_dir']		= 'Girdiginiz dizin yanlis yada mevcut degil.';
+$LNG['req_ftp_username']		= 'Kullanıcı Adı';
+$LNG['req_ftp_password']		= 'Şifre';
+$LNG['req_ftp_dir']				= '2Moons klasörü';
+$LNG['req_ftp_send']			= 'Gönder';
+$LNG['req_ftp_error_data']		= 'Verilen bilgiler ile FTP sunucusuna bağlanılamadı. Görev başarısız oldu.';
+$LNG['req_ftp_error_dir']		= 'Girdiğiniz dizin yanlış ya da mevcut degil.';
 
-$LNG['step1_head']				= 'Kurulum veritabanini konfigure et';
+$LNG['step1_head']				= 'Kurulum veritabanını konfigure et';
 $LNG['step1_desc']				= '2Moons sisteminize kurululabilir. Simdi, veritabanina baglanmak icin asagidaki bilgileri girmelisiniz. Eger gerekli bilgileri bilmiyorsaniz, domain (alan) saglayiciniz ile irtibata geciniz yada 2Moons forumlarindaki direktifleri okuyunuz.';
-$LNG['step1_mysql_server']		= 'Veritabani sinicisi <br /> SQL Server';
-$LNG['step1_mysql_port']		= 'Veritabani-Port';
-$LNG['step1_mysql_dbuser']		= 'Veritabani-Kullanici adi';
-$LNG['step1_mysql_dbpass']		= 'Veritabani-Sifre';
-$LNG['step1_mysql_dbname']		= 'Veritabani';
-$LNG['step1_mysql_prefix']		= 'Veritabani prefix eki: <br /> Degistirmeniz gerekmez';
+$LNG['step1_mysql_server']		= 'Veritabanı sunucusu <br /> SQL Server';
+$LNG['step1_mysql_port']		= 'Veritabanı-Port';
+$LNG['step1_mysql_dbuser']		= 'Veritabanı-Kullanıcı adı';
+$LNG['step1_mysql_dbpass']		= 'Veritabanı-Şifre';
+$LNG['step1_mysql_dbname']		= 'Veritabanı';
+$LNG['step1_mysql_prefix']		= 'Veritabanı prefix eki: <br /> Değiştirmeniz gerekmez';
 
 $LNG['step2_prefix_invalid']	= 'Prefix eki sadece alfanumerik karakterlerden olusur ve son karakter altcizgi ( _ ) olmalidir. ';
-$LNG['step2_db_no_dbname']		= 'Veritabani adi girmediniz.';
+$LNG['step2_db_no_dbname']		= 'Veritabano adı girmediniz.';
 $LNG['step2_db_too_long']		= 'Prefiks eki cok uzun. En fazla 36 hane olabilir.';
-$LNG['step2_db_con_fail']		= 'Veritabanina baglanirken hata olustu. Detaylar : ';
-$LNG['step2_conf_op_fail']		= "config.php yazilamadi!";
-$LNG['step2_conf_create']		= 'config.php basariyla olustu!';
+$LNG['step2_db_con_fail']		= 'Veritabanına baglanirken hata olustu. Detaylar : ';
+$LNG['step2_conf_op_fail']		= "config.php yazılamadı!";
+$LNG['step2_conf_create']		= 'config.php başarıyla oluştu!';
 $LNG['step2_config_exists']		= 'config.php zaten mevcut!';
-$LNG['step2_db_done']			= 'Basari ile veritabananina baglandi!';
+$LNG['step2_db_done']			= 'Başarı ile veritabananına bağlandı!';
 
-$LNG['step3_head']				= 'Veritabani dosyalari olusturuluyor';
-$LNG['step3_desc']				= '2Moons veritabani icin mevcut tablolar yaratildi ve varsayilan degerler yuklendi. Bir sonraki isleme gecip kurulumu tamamlamak icin;';
-$LNG['step3_db_error']			= 'Veritabani tablolari olusturulamadi:';
+$LNG['step3_head']				= 'Veritabanı dosyaları oluşturuluyor';
+$LNG['step3_desc']				= '2Moons veritabanı için mevcut tablolar yaratıldı ve varsayılan değerler yüklendi. Bir sonraki işleme geçip kurulumu tamamlamak için;';
+$LNG['step3_db_error']			= 'Veritabanı tabloları oluşturulamadı:';
 
-$LNG['step4_head']				= 'Admin Hesabi';
-$LNG['step4_desc']				= 'Kurulum sihirbazi simdi sizin icin admin hesabi yaratacak. Asagiya kullanici adi, sifre ve mail adresinizi yaziniz.';
-$LNG['step4_admin_name']		= 'Admin-kullanici adi:';
-$LNG['step4_admin_name_desc']	= '3-20 karakter arasi kullanici adi giriniz.';
-$LNG['step4_admin_pass']		= 'Admin sifresi:';
-$LNG['step4_admin_pass_desc']	= '6-30 karakter arasi sifre giriniz';
+$LNG['step4_head']				= 'Admin Hesabı';
+$LNG['step4_desc']				= 'Kurulum sihirbazi şimdi sizin için admin hesabı yaratacak. Aşağıya kullanıcı adı, şifre ve mail adresinizi yazınız.';
+$LNG['step4_admin_name']		= 'Admin-kullanıcı adı:';
+$LNG['step4_admin_name_desc']	= '3-20 karakter arası kullanıcı adı giriniz.';
+$LNG['step4_admin_pass']		= 'Admin şifresi:';
+$LNG['step4_admin_pass_desc']	= '6-30 karakter arasi şifre giriniz';
 $LNG['step4_admin_mail']		= 'E-mail adresi:';
 
-$LNG['step6_head']				= 'Kurulum basari ile tamamlandi!';
-$LNG['step6_desc']				= '2Moons sisteminize basari ile kuruldu';
-$LNG['step6_info_head']			= '2Moons kullanilmaya hazir!';
-$LNG['step6_info_additional']	= 'Asagidaki butona tiklayarak 2moons admin sayfasina yonlendirileceksiniz. 2Moons admin araclarini buradan inceleyebilirsiniz.<br/><br/><strong> &raquo;includes/ENABLE_INSTALL_TOOL&laquo; dosyasini silmeyi unutmayiniz. Eger bu dosya sisteminizde kalirsa baska birisinin oyunu tekrar kurmasina izin vererek kurulumunuzu buyuk riske atarsiniz!</strong>';
+$LNG['step6_head']				= 'Kurulum başaru ile tamamlandı!';
+$LNG['step6_desc']				= '2Moons sisteminize başarı ile kuruldu';
+$LNG['step6_info_head']			= '2Moons kullanılmaya hazır!';
+$LNG['step6_info_additional']	= 'Aşağıdaki butona tikladığınızda 2moons admin sayfasına yönlendirileceksiniz. 2Moons admin araçlarını buradan inceleyebilirsiniz.<br/><br/><strong> &raquo;includes/ENABLE_INSTALL_TOOL&laquo; dosyasini silmeyi unutmayiniz. Eger bu dosya sisteminizde kalirsa baska birisinin oyunu tekrar kurmasina izin vererek kurulumunuzu buyuk riske atarsiniz!</strong>';
 
-$LNG['sql_close_reason']		= 'Oyun su an kapali';
-$LNG['sql_welcome']				= '2Moons\'a hosgeldiniz';
+$LNG['sql_close_reason']		= 'Oyun şuanda kapalı';
+$LNG['sql_welcome']				= '2Moons\'a hoşgeldiniz';
 $LNG['reg_pdo_active']			= 'PDO Aktif';
-$LNG['reg_pdo_desc']			= 'PHP Veri Nesneleri (PDO) eklentisi, PHP\'deki veritabanlarina erismek için hafif ve tutarli bir arayuz tanimlar. PDO arayuzu tanimi bulunan her veritabani surucuusu, veritabanina ozgu ozellikleri siradan eklenti islevleri olarak ifade edebilir.';
-$LNG['step8_need_fields']		= 'Zorunlu alanlari doldurmadiniz. Lutfen geri giderek butnu alanlari doldurunuz!';
-
-
-
+$LNG['reg_pdo_desc']			= 'PHP Veri Nesneleri (PDO) eklentisi, PHP\'deki veritabanlarına erişmek için hafif ve tutarli bir arayüz tanımlar. PDO arayüzü tanımı bulunan her veritabanı sürücüsü, veritabanina özgü özellikleri sıradan eklenti işlevleri olarak ifade edebilir.';
+$LNG['step8_need_fields']		= 'Zorunlu alanları doldurmadınız. Lütfen geri giderek bütün alanları doldurunuz!';
