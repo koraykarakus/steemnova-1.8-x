37d621a671c8af69fa4fff928c2c11db51e660d9
koraykarakus <koraykarakus@yahoo.com>
2023-01-06 23:13:26 +0300

    Updated for new DB class

--- includes/pages/adm/ShowGiveawayPage.php
+++ includes/pages/adm/ShowGiveawayPage.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- *  2Moons 
+ *  2Moons
  *   by Jan-Otto Kröpke 2009-2016
  *
  * For the full copyright and license information, please view the LICENSE
@@ -14,77 +14,78 @@
  * @version 1.8.0
  * @link https://github.com/jkroepke/2Moons
  */
- 
+
  if (!allowedTo(str_replace(array(dirname(__FILE__), '\\', '/', '.php'), '', __FILE__))) throw new Exception("Permission error!");
 function ShowGiveaway()
 {
 	global $LNG, $resource, $reslist;
-	$template	= new template();	
+	$template	= new template();
 	$action	= HTTP::_GP('action', '');
 	if ($action == 'send') {
 		$planet			= HTTP::_GP('planet', 0);
 		$moon			= HTTP::_GP('moon', 0);
 		$mainplanet		= HTTP::_GP('mainplanet', 0);
 		$no_inactive	= HTTP::_GP('no_inactive', 0);
-		
+
 		if (!$planet && !$moon) {
 			$template->message($LNG['ga_selectplanettype']);
 			exit;
 		}
-		
+
 		$planetIN	= array();
-		
+
 		if ($planet) {
 			$planetIN[]	= "'1'";
-		} 
-		
+		}
+
 		if ($moon) {
 			$planetIN[]	= "'3'";
-		} 
-		
+		}
+
 		$data		= array();
-		
+
 		$DataIDs	= array_merge($reslist['resstype'][1], $reslist['resstype'][3], $reslist['build'], $reslist['tech'], $reslist['fleet'], $reslist['defense'], $reslist['officier']);
-		
+
 		$logOld		= array();
 		$logNew		= array();
-		
+
 		foreach($DataIDs as $ID)
 		{
 			$amount	= max(0, round(HTTP::_GP('element_'.$ID, 0.0)));
 			$data[]	= $resource[$ID]." = ".$resource[$ID]." + ".$amount;
-			
+
 			$logOld[$ID]	= 0;
 			$logNew[$ID]	= $amount;
 		}
-		
-		$SQL		= "UPDATE ".PLANETS." p INNER JOIN ".USERS." u ON p.id_owner = u.id";
-		
+
+		$SQL		= "UPDATE %%PLANETS%% p INNER JOIN %%USERS%% u ON p.id_owner = u.id";
+
 		if ($mainplanet == true) {
 			$SQL	.= " AND p.id = u.id_planet";
 		}
-		
+
 		if ($no_inactive == true) {
 			$SQL	.= " AND u.onlinetime > ".(TIMESTAMP - INACTIVE);
 		}
-		
-		$SQL	.= " SET ".implode(', ', $data)." WHERE p.universe = ".Universe::getEmulated()." AND p.planet_type IN (".implode(',', $planetIN).")";
-		
-		$GLOBALS['DATABASE']->query($SQL);
-		
+
+		$SQL	.= " SET ".implode(', ', $data)." WHERE p.universe = :universe AND p.planet_type IN (".implode(',', $planetIN).")";
+
+		Database::get()->update($SQL,array(
+      ':universe' => Universe::getEmulated()
+    ));
+
 		$LOG = new Log(4);
 		$LOG->target = 0;
 		$LOG->old = $logOld;
 		$LOG->new = $logNew;
 		$LOG->save();
-		
+
 		$template->message($LNG['ga_success']);
 		exit;
-	}	
-	
-	$template->assign_vars(array(	
+	}
+
+	$template->assign_vars(array(
 		'reslist'		=> $reslist
 	));
 	$template->show("giveaway.tpl");
 }
-
