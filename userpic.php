7487501574e43bf6380e6bf2c3064dd2db0f9034
koraykarakus <koraykarakus@yahoo.com>
2026-02-24 18:48:40 +0300

    code style update userpic.php

--- userpic.php
+++ userpic.php
@@ -16,42 +16,46 @@
  */
 
 define('MODE', 'BANNER');
-define('ROOT_PATH', str_replace('\\', '/',dirname(__FILE__)).'/');
+define('ROOT_PATH', str_replace('\\', '/', dirname(__FILE__)).'/');
 set_include_path(ROOT_PATH);
 
-if(!extension_loaded('gd'))
+require 'includes/common.php';
+
+if (!extension_loaded('gd'))
 {
-	clearGIF();
+    clearGIF();
 }
 
-require 'includes/common.php';
 $id = HTTP::_GP('id', 0);
 
-if(!isModuleAvailable(MODULE_BANNER) || $id == 0)
+if (!isModuleAvailable(MODULE_BANNER)
+    || $id == 0)
 {
-	clearGIF();
+    clearGIF();
 }
 
-$LNG = new Language;
+$LNG = new Language();
 $LNG->getUserAgentLanguage();
-$LNG->includeData(array('L18N', 'BANNER', 'CUSTOM'));
+$LNG->includeData(['L18N', 'BANNER', 'CUSTOM']);
 
 require 'includes/classes/class.StatBanner.php';
 
 $banner = new StatBanner();
 $Data = $banner->GetData($id);
-if(!isset($Data) || !is_array($Data))
+if (!isset($Data)
+    || !is_array($Data))
 {
-	clearGIF();
+    clearGIF();
 }
 
 $ETag = md5(implode('', $Data));
 header('ETag: '.$ETag);
 
-if(isset($_SERVER['HTTP_IF_NONE_MATCH']) && $_SERVER['HTTP_IF_NONE_MATCH'] == $ETag)
+if (isset($_SERVER['HTTP_IF_NONE_MATCH'])
+    && $_SERVER['HTTP_IF_NONE_MATCH'] == $ETag)
 {
-	HTTP::sendHeader('HTTP/1.0 304 Not Modified');
-	exit;
+    HTTP::sendHeader('HTTP/1.0 304 Not Modified');
+    exit;
 }
 
 $banner->CreateUTF8Banner($Data);
