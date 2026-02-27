de982bc969ffebc1ba030c460a79bb99af606883
koraykarakus <koraykarakus@yahoo.com>
2026-02-27 20:27:40 +0300

    Code style update ShowFleetMissilePage, spacing + snake case

--- includes/pages/game/ShowFleetMissilePage.class.php
+++ includes/pages/game/ShowFleetMissilePage.class.php
@@ -28,30 +28,31 @@ class ShowFleetMissilePage extends AbstractGamePage
     {
         global $USER, $PLANET, $LNG, $reslist, $resource;
 
-        $targetGalaxy = HTTP::_GP('galaxy', 0);
-        $targetSystem = HTTP::_GP('system', 0);
-        $targetPlanet = HTTP::_GP('planet', 0);
-        $targetType = HTTP::_GP('type', 0);
+        $target_galaxy = HTTP::_GP('galaxy', 0);
+        $target_system = HTTP::_GP('system', 0);
+        $target_planet = HTTP::_GP('planet', 0);
+        $target_type = HTTP::_GP('type', 0);
         $anz = min(HTTP::_GP('SendMI', 0), $PLANET['interplanetary_misil']);
-        $primaryTarget = HTTP::_GP('Target', 0);
+        $primary_target = HTTP::_GP('Target', 0);
 
         $db = Database::get();
 
         $sql = "SELECT id, id_owner FROM %%PLANETS%%
-        WHERE universe = :universe AND galaxy = :targetGalaxy
-        AND system = :targetSystem AND planet = :targetPlanet AND planet_type = :targetType;";
+        WHERE universe = :universe AND galaxy = :target_galaxy
+        AND system = :target_system AND planet = :target_planet 
+        AND planet_type = :target_type;";
 
         $target = $db->selectSingle($sql, [
-            ':universe'     => Universe::current(),
-            ':targetGalaxy' => $targetGalaxy,
-            ':targetSystem' => $targetSystem,
-            ':targetPlanet' => $targetPlanet,
-            ':targetType'   => $targetType,
+            ':universe'      => Universe::current(),
+            ':target_galaxy' => $target_galaxy,
+            ':target_system'  => $target_system,
+            ':target_planet'  => $target_planet,
+            ':target_type'    => $target_type,
         ]);
 
-        $Range = FleetFunctions::GetMissileRange($USER[$resource[117]]);
-        $systemMin = $PLANET['system'] - $Range;
-        $systemMax = $PLANET['system'] + $Range;
+        $range = FleetFunctions::GetMissileRange($USER[$resource[117]]);
+        $systemMin = $PLANET['system'] - $range;
+        $systemMax = $PLANET['system'] + $range;
 
         $error = [];
 
@@ -70,7 +71,9 @@ class ShowFleetMissilePage extends AbstractGamePage
             $error[] = $LNG['ma_impulse_drive_required'];
         }
 
-        if ($targetGalaxy != $PLANET['galaxy'] || $targetSystem < $systemMin || $targetSystem > $systemMax)
+        if ($target_galaxy != $PLANET['galaxy']
+            || $target_system < $systemMin
+            || $target_system > $systemMax)
         {
             $error[] = $LNG['ma_not_send_other_galaxy'];
         }
@@ -80,7 +83,8 @@ class ShowFleetMissilePage extends AbstractGamePage
             $error[] = $LNG['ma_planet_doesnt_exists'];
         }
 
-        if (!in_array($primaryTarget, $reslist['defense']) && $primaryTarget != 0)
+        if (!in_array($primary_target, $reslist['defense']) 
+            && $primary_target != 0)
         {
             $error[] = $LNG['ma_wrong_target'];
         }
@@ -97,27 +101,29 @@ class ShowFleetMissilePage extends AbstractGamePage
 
         if (empty($target))
         {
+            $target = [];
             $target['id_owner'] = 0;
-            $targetUser = ['onlinetime' => 0, 'banaday' => 0, 'urlaubs_modus' => 0, 'authattack' => 0];
+            $target_user = ['onlinetime' => 0, 'banaday' => 0, 'urlaubs_modus' => 0, 'authattack' => 0];
         }
         else
         {
-            $targetUser = GetUserByID($target['id_owner'], ['onlinetime', 'banaday', 'urlaubs_modus', 'authattack']);
+            $target_user = GetUserByID($target['id_owner'], ['onlinetime', 'banaday', 'urlaubs_modus', 'authattack']);
         }
 
-        if (Config::get()->adm_attack == 1 && $targetUser['authattack'] > $USER['authlevel'])
+        if (Config::get()->adm_attack == 1 
+            && $target_user['authattack'] > $USER['authlevel'])
         {
             $error[] = $LNG['fl_admin_attack'];
         }
 
-        if ($targetUser['urlaubs_modus'])
+        if ($target_user['urlaubs_modus'])
         {
             $error[] = $LNG['fl_in_vacation_player'];
         }
 
         $sql = "SELECT total_points FROM %%USER_POINTS%% WHERE id_owner = :ownerId;";
 
-        $User2Points = $db->selectSingle($sql, [
+        $user2points = $db->selectSingle($sql, [
             ':ownerId' => $target['id_owner'],
         ]);
 
@@ -129,40 +135,42 @@ class ShowFleetMissilePage extends AbstractGamePage
             ':userId' => $USER['id'],
         ]);
 
-        $IsNoobProtec = CheckNoobProtec($USER, $User2Points, $targetUser);
+        $is_noob_protect = CheckNoobProtec($USER, $user2points, $target_user);
 
-        if ($IsNoobProtec['NoobPlayer'])
+        if ($is_noob_protect['NoobPlayer'])
         {
             $error[] = $LNG['fl_week_player'];
         }
 
-        if ($IsNoobProtec['StrongPlayer'])
+        if ($is_noob_protect['StrongPlayer'])
         {
             $error[] = $LNG['fl_strong_player'];
         }
 
         if (!empty($error))
         {
-            $errorText = "";
-            foreach ($error as $currentErrorText)
+            $error_text = "";
+            foreach ($error as $c_text)
             {
-                $errorText .= $currentErrorText . "\n";
+                $error_text .= $c_text . "\n";
             }
 
-            $this->printMessage($errorText);
+            $this->printMessage($error_text);
         }
 
-        $Duration = FleetFunctions::GetMIPDuration($PLANET['system'], $targetSystem);
+        $duration = FleetFunctions::GetMIPDuration($PLANET['system'], $target_system);
 
-        $DefenseLabel = ($primaryTarget == 0) ? $LNG['ma_all'] : $LNG['tech'][$primaryTarget];
+        $defense_label = ($primary_target == 0) ? 
+                        $LNG['ma_all'] : 
+                        $LNG['tech'][$primary_target];
 
-        $fleetArray = [503 => $anz];
+        $fleet_array = [503 => $anz];
 
-        $fleetStartTime = TIMESTAMP + $Duration;
-        $fleetStayTime = $fleetStartTime;
-        $fleetEndTime = $fleetStartTime;
+        $fleet_start_time = TIMESTAMP + $duration;
+        $fleet_stay_time = $fleet_start_time;
+        $fleet_end_time = $fleet_start_time;
 
-        $fleetResource = [
+        $fleet_resource = [
             901 => 0,
             902 => 0,
             903 => 0,
@@ -172,7 +180,7 @@ class ShowFleetMissilePage extends AbstractGamePage
         $this->save();
 
         FleetFunctions::sendFleet(
-            $fleetArray,
+            $fleet_array,
             10,
             $USER['id'],
             $PLANET['id'],
@@ -182,18 +190,18 @@ class ShowFleetMissilePage extends AbstractGamePage
             $PLANET['planet_type'],
             $target['id_owner'],
             $target['id'],
-            $targetGalaxy,
-            $targetSystem,
-            $targetPlanet,
-            $targetType,
-            $fleetResource,
-            $fleetStartTime,
-            $fleetStayTime,
-            $fleetEndTime,
+            $target_galaxy,
+            $target_system,
+            $target_planet,
+            $target_type,
+            $fleet_resource,
+            $fleet_start_time,
+            $fleet_stay_time,
+            $fleet_end_time,
             0,
-            $primaryTarget
+            $primary_target
         );
 
-        $this->printMessage("<b>".$anz."</b>". $LNG['ma_missiles_sended'].$DefenseLabel);
+        $this->printMessage("<b>".$anz."</b>". $LNG['ma_missiles_sended'].$defense_label);
     }
 }
