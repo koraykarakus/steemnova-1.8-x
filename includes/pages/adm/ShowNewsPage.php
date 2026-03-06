39fb804b0a0f2a4f876da6083c9b196613e39c22
koraykarakus <koraykarakus@yahoo.com>
2026-03-06 22:22:01 +0300

    cleanup + fix for admin/NewsPage

--- includes/pages/adm/ShowNewsPage.php
+++ includes/pages/adm/ShowNewsPage.php
@@ -27,31 +27,34 @@ class ShowNewsPage extends AbstractAdminPage
 
     public function show(): void
     {
-
         global $LNG, $USER;
 
         $db = Database::get();
-
         $sql = "SELECT * FROM %%NEWS%% ORDER BY id ASC;";
-
         $news = $db->select($sql);
 
         $news_list = [];
-
         foreach ($news as $c_news)
         {
             $news_list[] = [
-                'id'      => $c_news['id'],
-                'title'   => $c_news['title'],
-                'date'    => _date($LNG['php_tdformat'], $c_news['date'], $USER['timezone']),
+                'id'    => $c_news['id'],
+                'title' => $c_news['title'],
+                'date'  => _date(
+                    $LNG['php_tdformat'],
+                    $c_news['date'],
+                    $USER['timezone']
+                ),
                 'user'    => $c_news['user'],
                 'confirm' => sprintf($LNG['nws_confirm'], $c_news['title']),
             ];
         }
 
         $this->assign([
-            'NewsList'  => $news_list,
-            'nws_total' => sprintf($LNG['nws_total'], $news_list && count($news_list)),
+            'news_list'  => $news_list,
+            'news_total' => sprintf(
+                $LNG['nws_total'],
+                $news_list && count($news_list)
+            ),
         ]);
 
         $this->display('page.news.default.tpl');
@@ -67,7 +70,6 @@ class ShowNewsPage extends AbstractAdminPage
         ]);
 
         $this->display('page.news.create.tpl');
-
     }
 
     public function createSend(): void
@@ -80,17 +82,16 @@ class ShowNewsPage extends AbstractAdminPage
         $db = Database::get();
 
         $sql = "INSERT INTO %%NEWS%% (`id`, `user`, `date`, `title`, `text`) VALUES
-		(NULL, :userName, :actionTime, :title, :newsText);";
+		(NULL, :user_name, :action_time, :title, :news_text);";
 
         $db->insert($sql, [
-            ':userName'   => $USER['username'],
-            ':actionTime' => TIMESTAMP,
-            ':title'      => $title,
-            ':newsText'   => $text,
+            ':user_name'   => $USER['username'],
+            ':action_time' => TIMESTAMP,
+            ':title'       => $title,
+            ':news_text'   => $text,
         ]);
 
         $this->show();
-
     }
 
     public function delete(): void
@@ -112,15 +113,20 @@ class ShowNewsPage extends AbstractAdminPage
 
         $db = Database::get();
 
-        $sql = "SELECT id, title, text FROM %%NEWS%% WHERE id = :id;";
+        $sql = "SELECT `id`, `title`, `text` FROM %%NEWS%% WHERE `id` = :id;";
 
         $news = $db->selectSingle($sql, [
             ':id' => HTTP::_GP('id', 0),
         ]);
 
+        if ($news === false)
+        {
+            $this->printMessage('wrong id!');
+        }
+
         $this->assign([
             'mode'       => 1,
-            'nws_head'   => sprintf($LNG['nws_head_edit'], $news['title']),
+            'news_head'  => sprintf($LNG['nws_head_edit'], $news['title']),
             'news_id'    => $news['id'],
             'news_title' => $news['title'],
             'news_text'  => $news['text'],
@@ -132,24 +138,24 @@ class ShowNewsPage extends AbstractAdminPage
 
     public function editSend(): void
     {
-
         $db = Database::get();
 
         $title = HTTP::_GP('title', '', true);
         $text = HTTP::_GP('text', '', true);
         $edit_id = HTTP::_GP('id', 0);
 
-        $sql = "UPDATE %%NEWS%% SET `title` = :title, `text` = :newsText, `date` = :actionTime WHERE `id` = :edit_id LIMIT 1;";
+        $sql = "UPDATE %%NEWS%% SET `title` = :title, 
+        `text` = :news_text, `date` = :action_time 
+        WHERE `id` = :edit_id;";
 
         $db->update($sql, [
-            ':title'      => $title,
-            ':newsText'   => $text,
-            ':actionTime' => TIMESTAMP,
-            ':edit_id'    => $edit_id,
+            ':title'       => $title,
+            ':news_text'   => $text,
+            ':action_time' => TIMESTAMP,
+            ':edit_id'     => $edit_id,
         ]);
 
         $this->show();
-
     }
 
 }
--- styles/templates/adm/page.news.default.tpl
+++ styles/templates/adm/page.news.default.tpl
@@ -1,48 +1,60 @@
-
 {block name="content"}
-
-{nocache}
-{if isset($mode)}
-<form method="POST" action="?page=news&amp;action=send&amp;mode={$mode}">
-{if isset($news_id)}<input name="id" type="hidden" value="{$news_id}">{/if}
-<table class="table table-dark table-striped table-sm fs-12 w-50 my-5 mx-auto">
-<tr>
-	<th colspan="2">{$nws_head}</th>
-</tr>
-<tr>
-<tr>
-	<td width="25%">{$LNG.nws_title}</td><td><input type="text" name="title" value="{if isset($news_title)}{$news_title}{/if}"></td>
-</tr>
-<tr>
-	<td>{$LNG.nws_content}</td><td><textarea cols="70" rows="10" name="text">{if isset($news_text)}{$news_text}{/if}</textarea></td>
-</tr>
-<tr>
-	<td colspan="2"><input type="submit" name="Submit" value="{$LNG.button_submit}"></td>
-</tr>
-</table>
-</form>
-{/if}
-{/nocache}
-<table class="table table-dark table-striped table-sm fs-12 my-5 mx-auto">
-<tr>
-	<th colspan="5">{$LNG.nws_news}</thd>
-</tr>
-<tr>
-	<td>{$LNG.nws_id}</td>
-	<td>{$LNG.nws_title}</td>
-	<td>{$LNG.nws_date}</td>
-	<td>{$LNG.nws_from}</td>
-	<td>{$LNG.nws_del}</td>
-</tr>
-{foreach name=NewsList item=NewsRow from=$NewsList}<tr>
-<td><a href="?page=news&amp;mode=edit&amp;id={$NewsRow.id}">{$NewsRow.id}</a></td>
-<td><a href="?page=news&amp;mode=edit&amp;id={$NewsRow.id}">{$NewsRow.title}</a></td>
-<td><a href="?page=news&amp;mode=edit&amp;id={$NewsRow.id}">{$NewsRow.date}</a></td>
-<td><a href="?page=news&amp;mode=edit&amp;id={$NewsRow.id}">{$NewsRow.user}</a></td>
-<td><a href="?page=news&amp;mode=delete&amp;id={$NewsRow.id}" onclick="return confirm('{$NewsRow.confirm}');"><img border="0" src="./styles/resource/images/alliance/CLOSE.png" width="16" height="16"></a></td>
-</tr>
-{/foreach}
-<tr><td colspan="5"><a href="?page=news&amp;mode=create">{$LNG.nws_create}</a></td></tr>
-<tr><td colspan="5">{$nws_total}</td></tr>
-</table>
+	<form method="POST" action="?page=news&amp;mode=createSend">
+		{if isset($news_id)}<input name="id" type="hidden" value="{$news_id}">{/if}
+		<table class="table table-dark table-striped table-sm fs-12 w-50 my-5 mx-auto">
+			<tr>
+				<th colspan="2"></th>
+			</tr>
+			<tr>
+			<tr>
+				<td width="25%">{$LNG.nws_title}</td>
+				<td><input type="text" name="title" value="{if isset($news_title)}{$news_title}{/if}"></td>
+			</tr>
+			<tr>
+				<td>{$LNG.nws_content}</td>
+				<td><textarea cols="70" rows="10" name="text">{if isset($news_text)}{$news_text}{/if}</textarea></td>
+			</tr>
+			<tr>
+				<td colspan="2"><input type="submit" name="Submit" value="{$LNG.button_submit}"></td>
+			</tr>
+		</table>
+	</form>
+	<table class="table table-dark table-striped table-sm fs-12 my-5 mx-auto">
+		<tr>
+			<th colspan="5">{$LNG.nws_news}</thd>
+		</tr>
+		<tr>
+			<td>{$LNG.nws_id}</td>
+			<td>{$LNG.nws_title}</td>
+			<td>{$LNG.nws_date}</td>
+			<td>{$LNG.nws_from}</td>
+			<td>{$LNG.nws_del}</td>
+		</tr>
+		{foreach $news_list as $c_news}<tr>
+				<td>
+					<a href="?page=news&amp;mode=edit&amp;id={$c_news.id}">{$c_news.id}</a>
+				</td>
+				<td>
+					<a href="?page=news&amp;mode=edit&amp;id={$c_news.id}">{$c_news.title}</a>
+				</td>
+				<td>
+					<a href="?page=news&amp;mode=edit&amp;id={$c_news.id}">{$c_news.date}</a>
+				</td>
+				<td>
+					<a href="?page=news&amp;mode=edit&amp;id={$c_news.id}">{$c_news.user}</a>
+				</td>
+				<td>
+					<a href="?page=news&amp;mode=delete&amp;id={$c_news.id}" onclick="return confirm('{$c_news.confirm}');"><img
+							border="0" src="./styles/resource/images/alliance/CLOSE.png" width="16" height="16">
+					</a>
+				</td>
+			</tr>
+		{/foreach}
+		<tr>
+			<td colspan="5"><a href="?page=news&amp;mode=create">{$LNG.nws_create}</a></td>
+		</tr>
+		<tr>
+			<td colspan="5">{$news_total}</td>
+		</tr>
+	</table>
 {/block}
