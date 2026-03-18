608352387b543585128064b6e6f7235634fecb3c
koraykarakus <koraykarakus@yahoo.com>
2026-03-19 02:08:58 +0300

    drop ie7-9 support from exception handler

--- includes/GeneralFunctions.php
+++ includes/GeneralFunctions.php
@@ -649,11 +649,6 @@ function exceptionHandler($exception): void
     $DIR = MODE == 'INSTALL' ? '..' : '.';
     ob_start();
     echo '<!DOCTYPE html>
-    <!--[if lt IE 7 ]> <html lang="de" class="no-js ie6"> <![endif]-->
-    <!--[if IE 7 ]>    <html lang="de" class="no-js ie7"> <![endif]-->
-    <!--[if IE 8 ]>    <html lang="de" class="no-js ie8"> <![endif]-->
-    <!--[if IE 9 ]>    <html lang="de" class="no-js ie9"> <![endif]-->
-    <!--[if (gt IE 9)|!(IE)]><!--> <html lang="de" class="no-js"> <!--<![endif]-->
     <head>
 	<title>' . $gameName . ' - Error</title>
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
