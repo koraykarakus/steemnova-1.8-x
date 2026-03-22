9e29317ead7c3964d4ebddbd88713d183f54e983
koraykarakus <koraykarakus@yahoo.com>
2026-03-22 15:12:50 +0300

    restore galaxy.js time out behaviour

--- scripts/game/galaxy.js
+++ scripts/game/galaxy.js
@@ -14,8 +14,14 @@ function doit(missionID, planetID) {
 		if(messages.length == MaxFleetSetting) {
 			messages.filter(':last').remove();
 		}
-		var element		= $('<td />').attr('colspan', 8).attr('class', data.code == 600 ? "text-success text-center" : "text-danger text-center").text(data.mess).wrap('<tr />').parent();
+		var element	= $('<td/>').attr('colspan', 8).attr('class', data.code == 600 ? "text_center" : "text_center").text(data.mess).wrap('<tr/>').parent();
 		statustable.removeAttr('style').after(element);
+
+		setTimeout(function () {
+    		element.fadeOut(500, function () {
+        	element.remove();
+    	});
+		}, 3000);
 	});
 }
 
