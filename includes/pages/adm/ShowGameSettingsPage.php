7086af10f01b36a5647759dd6b686a5f2825a028
koraykarakus <koraykarakus@yahoo.com>
2026-03-31 22:29:22 +0300

    display rapid fire values on game settings page

--- includes/pages/adm/ShowGameSettingsPage.php
+++ includes/pages/adm/ShowGameSettingsPage.php
@@ -17,6 +17,23 @@ class ShowGameSettingsPage extends AbstractAdminPage
 
     public function rapidFire(): void
     {
+        global $LNG;
+        $db = Database::get();
+        $sql = "SELECT * FROM %%VARS_RAPIDFIRE%%;";
+        $rapid_fire_data = $db->select($sql,[]);
+
+        $rapid_fire_list = [];
+        foreach ($rapid_fire_data as $c_data) {
+            $rapid_fire_list[$c_data['element_id']][] = array(
+                'id' => $c_data['rapidfire_id'],
+                'shoots' => $c_data['shoots']
+            );
+        }
+
+        $this->assign([
+            'rapid_fire_list' => $rapid_fire_list,
+        ]);
+
         $this->display('page.gameSettings.rapidFire.tpl');
     }
 }
--- styles/templates/adm/page.gameSettings.rapidFire.tpl
+++ styles/templates/adm/page.gameSettings.rapidFire.tpl
@@ -1,5 +1,22 @@
 {block name="content"}
 <div class="bg-black w-75 p-3 my-3 mx-auto fs-12">
   <span class="fs-12 text-yellow fw-bold">RapidFire Settings</span>
+    {foreach $rapid_fire_list as $c_id => $c_val}
+      <table>
+        <thead>
+          <tr>
+            <td class="text-yellow">{$LNG.tech.{$c_id}}</td>
+          </tr>
+        </thead>
+        <tbody>
+          {foreach $c_val as $val}
+            <tr>
+              <td>{$LNG.tech.{$val.id}}</td>
+              <td>{$val.shoots}</td>
+            </tr>
+          {/foreach}
+        </tbody>
+      </table>
+    {/foreach}
 </div>
 {/block}
