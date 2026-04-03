2896548635060129c3c41f2dc8d95356acf8fcd1
koraykarakus <koraykarakus@yahoo.com>
2026-04-03 18:44:13 +0300

    calculateMIPAttack, naming convention update

--- includes/classes/missions/functions/calculateMIPAttack.php
+++ includes/classes/missions/functions/calculateMIPAttack.php
@@ -15,45 +15,54 @@
  * @link https://github.com/jkroepke/2Moons
  */
 
-function calculateMIPAttack($TargetDefTech, $OwnerAttTech, $missiles, $targetDefensive, $firstTarget, $defenseMissles)
-{
+function calculateMIPAttack(
+    $target_def_tech,
+    $owner_att_tech,
+    $missiles,
+    $target_defensive,
+    $first_target,
+    $defense_missiles
+) {
     global $PRICELIST, $COMBATCAPS;
 
-    $destroyShips = [];
-    $countMissles = $missiles - $defenseMissles;
+    $destroy_ships = [];
+    $count_missiles = $missiles - $defense_missiles;
 
-    if ($countMissles == 0)
+    if ($count_missiles == 0)
     {
-        return $destroyShips;
+        return $destroy_ships;
     }
 
-    $totalAttack = $countMissles * $COMBATCAPS[503]['attack'] * (1 + 0.1 * $OwnerAttTech);
+    $total_attack = $count_missiles * $COMBATCAPS[503]['attack'] * (1 + 0.1 * $owner_att_tech);
 
     // Select primary target, if exists
-    if (isset($targetDefensive[$firstTarget]))
+    if (isset($target_defensive[$first_target]))
     {
-        $firstTargetData = [$firstTarget => $targetDefensive[$firstTarget]];
-        unset($targetDefensive[$firstTarget]);
-        $targetDefensive = $firstTargetData + $targetDefensive;
+        $first_target_data = [$first_target => $target_defensive[$first_target]];
+        unset($target_defensive[$first_target]);
+        $target_defensive = $first_target_data + $target_defensive;
     }
 
-    foreach ($targetDefensive as $element => $count)
+    foreach ($target_defensive as $element => $count)
     {
         if ($element == 0)
         {
-            throw new Exception("Unknown error. Please report this error on tracker.2moons.cc. Debuginforations:<br><br>".serialize([$TargetDefTech, $OwnerAttTech, $missiles, $targetDefensive, $firstTarget, $defenseMissles]));
+            throw new Exception("Unknown error. Please report this error on tracker.2moons.cc. Debuginforations:<br><br>" .
+            serialize(
+                [$target_def_tech, $owner_att_tech, $missiles, $target_defensive, $first_target, $defense_missiles]
+            ));
         }
-        $elementStructurePoints = ($PRICELIST[$element]['cost'][901] + $PRICELIST[$element]['cost'][902]) * (1 + 0.1 * $TargetDefTech) / 10;
-        $destroyCount = floor($totalAttack / $elementStructurePoints);
+        $element_structure_points = ($PRICELIST[$element]['cost'][901] + $PRICELIST[$element]['cost'][902]) * (1 + 0.1 * $target_def_tech) / 10;
+        $destroyCount = floor($total_attack / $element_structure_points);
         $destroyCount = min($destroyCount, $count);
-        $totalAttack -= $destroyCount * $elementStructurePoints;
+        $total_attack -= $destroyCount * $element_structure_points;
 
-        $destroyShips[$element] = $destroyCount;
-        if ($totalAttack <= 0)
+        $destroy_ships[$element] = $destroyCount;
+        if ($total_attack <= 0)
         {
-            return $destroyShips;
+            return $destroy_ships;
         }
     }
 
-    return $destroyShips;
+    return $destroy_ships;
 }
