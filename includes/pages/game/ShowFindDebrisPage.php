2c114acd24130d96bebef7b73c3d0a76ab804aaa
koraykarakus <koraykarakus@yahoo.com>
2026-04-05 02:17:31 +0300

    initial commit, copy paste data from old module of steemnova

---
+++ includes/pages/game/ShowFindDebrisPage.php
@@ -0,0 +1,46 @@
+<?php
+
+class ShowFindDebrisPage extends AbstractGamePage
+{
+    public static $require_module = 0;
+
+	function __construct() 
+	{
+		parent::__construct();
+	}
+	
+	public function show(): void
+    {
+		global $USER, $PLANET, $resource, $pricelist;
+		
+		$mode = HTTP::_GP('y', '');
+		$table = "";
+		$range = $PLANET['hangar'] * 4;
+        if ($mode == '1')
+        {
+            $cautare = $GLOBALS['DATABASE']->query("SELECT *from ".PLANETS." where (`der_metal` >0 OR `der_crystal` >0) AND (`system` > '".($PLANET['system'] - $range)."' AND `system` < '".($PLANET['system'] + $range)."') AND `galaxy` = '".$PLANET['galaxy']."' and `planet_type` = '1' ;");
+                $table = "<table><tr><td>Galaxy</td><td>System</td><td>Planet</td><td>Debris Metal</td><td>Debris Crystal</td><td>Collect</td></tr>";
+                //print_r($cautare);
+            if($GLOBALS['DATABASE']->numRows($cautare) > 0)
+            while($GalaxyRowPlanet = $GLOBALS['DATABASE']->fetch_array($cautare)){
+            
+            $GRecNeeded = min(ceil(($GalaxyRowPlanet['der_metal'] + $GalaxyRowPlanet['der_crystal']) / $pricelist[219]['capacity']), $PLANET[$resource[219]]);
+            
+                $table .= "<tr><td>".$GalaxyRowPlanet['galaxy']."</td><td>".$GalaxyRowPlanet['system']."</td><td>".$GalaxyRowPlanet['planet']."</td><td>".$GalaxyRowPlanet['der_metal']."</td><td>".$GalaxyRowPlanet['der_crystal']."</td><td><a href='javascript:doit(8,".$GalaxyRowPlanet['id'].");'>Collect</a></td></tr>";
+            }
+            else
+            $table .= "<tr><td colspan='5'>There are no debris in your range</td></tr>";
+            $table .= "</table>";
+        }
+
+		$this->assign(
+			[
+				'range' => $range,
+				'debris' => $table,
+				'user_maxfleetsettings' => $USER['settings_fleetactions'],
+			]);
+
+		$this->display("page.findDebris.default.tpl");
+	}
+}
+?>
---
+++ styles/templates/game/page.findDebris.default.tpl
@@ -0,0 +1,43 @@
+{block name="title" prepend}{$LNG.lm_empire}{/block}
+{block name="content"}
+<table>
+        <tbody>
+			<tr>
+				<td><a href="?page=findDebris&y=1">Show Debris(range : {$range})</a></td>
+			</tr>
+				{$debris}
+		</tbody>
+		<table>
+		<tr style="display: none;" id="fleetstatusrow">
+		<th colspan="6">Fleet...</th>
+		</tr>
+		</table>
+	</tr>
+    </table>
+</div>
+<script type="text/javascript">
+		MaxFleetSetting = {$user_maxfleetsettings};
+	</script>
+<script>
+function doit(missionID, planetID) {
+	$.getJSON("game.php?page=fleetAjax&ajax=1&mission="+missionID+"&planetID="+planetID, function(data)
+	{
+		$('#slots').text(data.slots);
+		if(typeof data.ships !== "undefined")
+		{
+			$.each(data.ships, function(elementID, value) {
+				$('#elementID'+elementID).text(number_format(value));
+			});
+		}
+		
+		var statustable	= $('#fleetstatusrow');
+		var messages	= statustable.find("~tr");
+		if(messages.length == MaxFleetSetting) {
+			messages.filter(':last').remove();
+		}
+		var element		= $('<td />').attr('colspan', 8).attr('class', data.code == 600 ? "success" : "error").text(data.mess).wrap('<tr />').parent();
+		statustable.removeAttr('style').after(element);
+	});
+}
+</script>
+{/block}
