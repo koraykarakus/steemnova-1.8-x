cbd882a96c7f5934b921ade662aa296a71173804
koraykarakus <koraykarakus@yahoo.com>
2026-04-04 15:07:57 +0300

    MissionCaseStay to snake_case

--- includes/classes/missions/MissionCaseStay.class.php
+++ includes/classes/missions/MissionCaseStay.class.php
@@ -17,45 +17,60 @@
 
 class MissionCaseStay extends MissionFunctions implements Mission
 {
-    public function __construct($Fleet)
+    public function __construct($fleet)
     {
-        $this->_fleet = $Fleet;
+        $this->_fleet = $fleet;
     }
 
     public function TargetEvent()
     {
 
         $sql = 'SELECT * FROM %%USERS%% WHERE id = :userId;';
-        $senderUser = Database::get()->selectSingle($sql, [
+        $sender_user = Database::get()->selectSingle($sql, [
             ':userId' => $this->_fleet['fleet_owner'],
         ]);
 
-        $senderUser['factor'] = getFactors($senderUser, 'basic', $this->_fleet['fleet_start_time']);
+        $sender_user['factor'] = getFactors($sender_user, 'basic', $this->_fleet['fleet_start_time']);
 
-        $fleetArray = FleetFunctions::unserialize($this->_fleet['fleet_array']);
+        $fleet_array = FleetFunctions::unserialize($this->_fleet['fleet_array']);
         $duration = $this->_fleet['fleet_start_time'] - $this->_fleet['start_time'];
 
-        $SpeedFactor = FleetFunctions::GetGameSpeedFactor();
+        $speed_factor = FleetFunctions::GetGameSpeedFactor();
         $distance = FleetFunctions::GetTargetDistance(
             [$this->_fleet['fleet_start_galaxy'], $this->_fleet['fleet_start_system'], $this->_fleet['fleet_start_planet']],
             [$this->_fleet['fleet_end_galaxy'], $this->_fleet['fleet_end_system'], $this->_fleet['fleet_end_planet']]
         );
 
-        $consumption = FleetFunctions::GetFleetConsumption($fleetArray, $duration, $distance, $senderUser, $SpeedFactor);
+        $consumption = FleetFunctions::GetFleetConsumption(
+            $fleet_array,
+            $duration,
+            $distance,
+            $sender_user,
+            $speed_factor
+        );
 
         $this->UpdateFleet('fleet_resource_deuterium', $this->_fleet['fleet_resource_deuterium'] + $consumption / 2);
 
-        $LNG = $this->getLanguage($senderUser['lang']);
-        $TargetUserID = $this->_fleet['fleet_target_owner'];
-        $TargetMessage = sprintf($LNG['sys_stat_mess'], GetTargetAddressLink($this->_fleet, ''), pretty_number($this->_fleet['fleet_resource_metal']), $LNG['tech'][901], pretty_number($this->_fleet['fleet_resource_crystal']), $LNG['tech'][902], pretty_number($this->_fleet['fleet_resource_deuterium']), $LNG['tech'][903]);
+        $LNG = $this->getLanguage($sender_user['lang']);
+        $target_user_id = $this->_fleet['fleet_target_owner'];
+        $target_message = sprintf(
+            $LNG['sys_stat_mess'],
+            GetTargetAddressLink($this->_fleet, ''),
+            pretty_number($this->_fleet['fleet_resource_metal']),
+            $LNG['tech'][901],
+            pretty_number($this->_fleet['fleet_resource_crystal']),
+            $LNG['tech'][902],
+            pretty_number($this->_fleet['fleet_resource_deuterium']),
+            $LNG['tech'][903]
+        );
 
         PlayerUtil::sendMessage(
-            $TargetUserID,
+            $target_user_id,
             0,
             $LNG['sys_mess_tower'],
             5,
             $LNG['sys_stat_mess_stay'],
-            $TargetMessage,
+            $target_message,
             $this->_fleet['fleet_start_time'],
             null,
             1,
@@ -76,7 +91,7 @@ class MissionCaseStay extends MissionFunctions implements Mission
     {
         $LNG = $this->getLanguage(null, $this->_fleet['fleet_owner']);
 
-        $Message = sprintf(
+        $message = sprintf(
             $LNG['sys_stat_mess'],
             GetStartAddressLink($this->_fleet, ''),
             pretty_number($this->_fleet['fleet_resource_metal']),
@@ -93,7 +108,7 @@ class MissionCaseStay extends MissionFunctions implements Mission
             $LNG['sys_mess_tower'],
             4,
             $LNG['sys_mess_fleetback'],
-            $Message,
+            $message,
             $this->_fleet['fleet_end_time'],
             null,
             1,
